e5039bc91bdb2ce9ab4b2734a0fd88370fcc5fab
Gonzasch35 <gonza.sch35@gmail.com>
2024-07-23 23:03:29 -0300

    commit

--- papercraft/models.py
+++ papercraft/models.py
@@ -17,6 +17,7 @@ class Modelo(models.Model):
     ancho = models.CharField(max_length=20)
     profundidad = models.CharField(max_length=20)
     piezas = models.CharField(max_length=20)
+    
 
     def __str__(self):
         return self.name
